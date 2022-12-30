be9f9daf56f0c1af59d0d416dd938a41eff943b0
Shannon <shannon.m.pitts@gmail.com>
2022-12-30 17:35:07 -0500

    settings

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -35,8 +35,6 @@ from evennia.settings_default import *
 SERVERNAME = "perennia"
 MULTISESSION_MODE = 3
 IN_GAME_ERRORS = False
-SERVER_HOSTNAME = '159.223.163.199'
-WEBSOCKET_CLIENT_INTERFACE = '159.223.163.199'
 
 
 ######################################################################
